5084cd7e2f6fafe7087be6559fd1da27b128632d
ChaoF <alanackart@gmail.com>
2020-08-27 21:09:54 +0800

    Update desktopBackgroundChanger.py
    Persistently change Windows desktop

--- desktopBackgroundChanger.py
+++ desktopBackgroundChanger.py
@@ -22,7 +22,9 @@ def check_image(p):
 
 def change_background(item):
     print(datetime.datetime.now(), str(item))
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, str(item), 0)
+    SPI_SETDESKWALLPAPER = 20
+    SPIF_UPDATEINIFILE = 1
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, str(item), SPIF_UPDATEINIFILE)
 
 
 def job(files_grabbed):
